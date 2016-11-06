ab61ee59cf2abddd0ff48c5845c954b1332e0c33
Damilola JOEL <djdamilola01@gmail.com>
2016-11-06 22:54:56 +0100

    changed the redited_to after user login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {   
-        return view('home');
+        return view('users.index');
     }
 
     public function test()
